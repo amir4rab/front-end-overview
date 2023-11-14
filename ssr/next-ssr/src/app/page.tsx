9c1eceff4b37167700b0bcd5de8b88b815207625
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-14 07:28:33 +0000

    added list component and data fetching

--- ssr/next-ssr/src/app/page.tsx
+++ ssr/next-ssr/src/app/page.tsx
@@ -1,3 +1,13 @@
+// Types
+import { Cities } from "@/type";
+
+// Components
+import List from "@/components/list";
+
+// Forcing the page to be fully dynamic
+export const dynamic = "force-dynamic";
+
+// Data Fetching
 async function getData() {
   const date = new Date();
   const renderTime = `${date.getUTCHours().toString().padStart(2, "0")}:${date
@@ -11,18 +21,32 @@ async function getData() {
     .toString()
     .padStart(2, "0")}`;
 
-  return { renderTime };
+  const { cities, credit, license } = (await fetch("http://localhost:3090", {
+    cache: "no-store",
+  }).then((data) => data.json())) as {
+    cities: Cities;
+    license: string;
+    credit: string;
+  };
+
+  return { renderTime, cities, credit, license };
 }
 
 export default async function Home() {
-  const { renderTime } = await getData();
+  const { renderTime, cities, credit, license } = await getData();
 
   return (
     <main>
       <p className="bg-black text-white font-mono text-center py-2 text-sm">
         Rendered at {renderTime}
       </p>
-      <h1>Hello world</h1>
+      <p className="max-w-3xl rounded-3xl mt-6 px-8 py-6 bg-neutral-100 font-mono mx-auto">
+        {license}
+      </p>
+      <List cities={cities} />
+      <p className="max-w-3xl rounded-3xl mt-6 px-8 py-6 bg-yellow-200/50 shadow-yellow-200/10 shadow-xl text-xs border border-yellow-200 font-mono mx-auto">
+        All the ciredit for the provided data goes to {credit}
+      </p>
     </main>
   );
 }
