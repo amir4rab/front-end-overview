e9533df6d5b282016b6613efe51c17013d41e99c
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-19 05:18:01 +0000

    added node adaptor to astro

--- ssr/astro-ssr/astro.config.mjs
+++ ssr/astro-ssr/astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 
+// Integrations
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
+// Adaptors
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [
     tailwind(),
     preact({
@@ -19,4 +24,7 @@ export default defineConfig({
       include: ["**/solid/*"],
     }),
   ],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
