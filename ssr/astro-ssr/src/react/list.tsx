063ea20da81adf0db673312f5f9b801583e73868
Amir <60965542+amir4rab@users.noreply.github.com>
2023-11-25 14:22:23 +0000

    added item hiding

--- ssr/astro-ssr/src/react/list.tsx
+++ ssr/astro-ssr/src/react/list.tsx
@@ -7,24 +7,40 @@ interface Props {
   cities: Cities;
 }
 
+interface Props {
+  cities: Cities;
+}
+
 const List = ({ cities }: Props) => {
   const [query, setQuery] = useState("");
+  const [hiddenItems, setHiddenItem] = useState<Record<string, boolean>>({});
 
   const filteredList = useMemo(() => {
-    return cities.filter(({ name }) =>
-      query === "" ? true : name.toLowerCase().includes(query)
-    );
-  }, [query, cities]);
+    return cities.filter(({ name, population }) => {
+      if (hiddenItems[name + population]) return false;
+      return query === "" ? true : name.toLowerCase().includes(query);
+    });
+  }, [hiddenItems, query]);
 
   return (
     <div className="max-w-3xl mx-auto my-12">
       <form className="bg-neutral-900 text-white px-6 py-4 rounded-3xl grid gap-2 shadow-neutral-900/20 shadow-xl">
-        <label className="text-2xl font-semibold" htmlFor="search">
-          Search
-        </label>
+        <div className="flex justify-between">
+          <label className="text-2xl font-semibold" htmlFor="search">
+            Search
+          </label>
+          {Object.values(hiddenItems).length !== 0 && (
+            <button
+              onClick={() => setHiddenItem({})}
+              className="text-sm underline hover:text-green-200 transition-colors duration-200"
+            >
+              Clear {Object.values(hiddenItems).length} hidden items
+            </button>
+          )}
+        </div>
         <input
           className="rounded-xl px-4 py-2 bg-neutral-700 text-white"
-          onKeyDown={(e) =>
+          onKeyUp={(e) =>
             setQuery((e.target as HTMLInputElement).value.toLowerCase())
           }
         />
@@ -32,10 +48,38 @@ const List = ({ cities }: Props) => {
       <ol className="grid gap-2 my-4">
         {filteredList.map(({ name, population }) => (
           <li
-            key={name}
-            className="grid gap-2 px-6 py-4 rounded-3xl bg-neutral-100"
+            key={name + population}
+            data-hidden={hiddenItems?.[name + population]}
+            className={[
+              "grid",
+              "gap-2",
+              "px-6",
+              "py-4",
+              "rounded-3xl",
+              "bg-neutral-100",
+              "data-[hidden=true]:hidden",
+              "shadow-xl",
+              "shadow-transparent",
+              "hover:shadow-neutral-900/10",
+              "hover:scale-[101%]",
+              "transition-[box-shadow,transform]",
+              "duration-150",
+            ].join(" ")}
           >
-            <p className="text-lg font-semibold">{name}</p>
+            <div className="flex justify-between">
+              <p className="text-lg font-semibold">{name}</p>
+              <button
+                className="px-4 py-1 rounded-3xl bg-neutral-200 text-sm capitalize hover:bg-neutral-300 transition-color duration-150"
+                onClick={() => {
+                  setHiddenItem((curr) => ({
+                    ...curr,
+                    [name + population]: true,
+                  }));
+                }}
+              >
+                hide
+              </button>
+            </div>
             <p className="text-sm opacity-75 font-mono uppercase">
               population: {population.toLocaleString("en-US")}
             </p>
--- ssr/solid-ssr/src/components/list.tsx
+++ ssr/solid-ssr/src/components/list.tsx
@@ -9,22 +9,36 @@ interface Props {
 
 const List = ({ cities }: Props) => {
   const [query, setQuery] = createSignal("");
+  const [hiddenItems, setHiddenItem] = createSignal<Record<string, boolean>>(
+    {}
+  );
 
   const filteredList = createMemo(() => {
-    return cities.filter(({ name }) =>
-      query() === "" ? true : name.toLowerCase().includes(query())
-    );
+    return cities.filter(({ name, population }) => {
+      if (hiddenItems()[name + population]) return false;
+      return query() === "" ? true : name.toLowerCase().includes(query());
+    });
   });
 
   return (
     <div class="max-w-3xl mx-auto my-12">
       <form class="bg-neutral-900 text-white px-6 py-4 rounded-3xl grid gap-2 shadow-neutral-900/20 shadow-xl">
-        <label class="text-2xl font-semibold" for="search">
-          Search
-        </label>
+        <div class="flex justify-between">
+          <label class="text-2xl font-semibold" for="search">
+            Search
+          </label>
+          {Object.values(hiddenItems()).length !== 0 && (
+            <button
+              onClick={() => setHiddenItem({})}
+              class="text-sm underline hover:text-green-200 transition-colors duration-200"
+            >
+              Clear {Object.values(hiddenItems()).length} hidden items
+            </button>
+          )}
+        </div>
         <input
           class="rounded-xl px-4 py-2 bg-neutral-700 text-white"
-          onKeyDown={(e) =>
+          onKeyUp={(e) =>
             setQuery((e.target as HTMLInputElement).value.toLowerCase())
           }
         />
@@ -39,8 +53,38 @@ const List = ({ cities }: Props) => {
           }
         >
           {({ name, population }) => (
-            <li class="grid gap-2 px-6 py-4 rounded-3xl bg-neutral-100">
-              <p class="text-lg font-semibold">{name}</p>
+            <li
+              data-hidden={hiddenItems()?.[name + population]}
+              class={[
+                "grid",
+                "gap-2",
+                "px-6",
+                "py-4",
+                "rounded-3xl",
+                "bg-neutral-100",
+                "data-[hidden=true]:hidden",
+                "shadow-xl",
+                "shadow-transparent",
+                "hover:shadow-neutral-900/10",
+                "hover:scale-[101%]",
+                "transition-[box-shadow,transform]",
+                "duration-150",
+              ].join(" ")}
+            >
+              <div class="flex justify-between">
+                <p class="text-lg font-semibold">{name}</p>
+                <button
+                  class="px-4 py-1 rounded-3xl bg-neutral-200 text-sm capitalize hover:bg-neutral-300 transition-color duration-150"
+                  onClick={() => {
+                    setHiddenItem((curr) => ({
+                      ...curr,
+                      [name + population]: true,
+                    }));
+                  }}
+                >
+                  hide
+                </button>
+              </div>
               <p class="text-sm opacity-75 font-mono uppercase">
                 population: {population.toLocaleString("en-US")}
               </p>
